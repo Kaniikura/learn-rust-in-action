b1270c983a7a4f36207f8f16aa724414aca9621a
Kaniikura <constantl492@gmail.com>
2022-03-15 01:56:26 +0900

    Sec. #2.6

---
+++ ch02/ch02-needle-in-haystack/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let needle = 0o64;
+    let haystack = [1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147];
+
+    for item in &haystack {
+        // haystack配列に含まれる各要素への参照を反復処理する
+        if *item == needle {
+            // *itemという構文は、itemの参照先を返す
+            println!("{}", item);
+        }
+    }
+}
